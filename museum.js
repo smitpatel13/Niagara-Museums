65065371d99504a8422a235fe65c1366ad6f892f
smitpatel13 <47191362+smitpatel13@users.noreply.github.com>
2019-02-04 09:56:09 -0500

    Add files via upload

---
+++ museum.js
@@ -0,0 +1,272 @@
+var museumList =  [
+  {
+    "ID": 15,
+    "NAME": "Mackenzie Printery",
+    "MUNICIPALITY": "Niagara-on-the-Lake",
+    "ADDRESS": "1 Queenston St",
+    "LONGITUDE": -79.05384828,
+    "LATITUDE": 43.16224036,
+    "PHONE": "(905) 262-5676"
+  },
+  {
+    "ID": 4,
+    "NAME": "RiverBrink Art Museum",
+    "MUNICIPALITY": "Niagara-on-the-Lake",
+    "ADDRESS": "116 Queenston St",
+    "LONGITUDE": -79.05700157,
+    "LATITUDE": 43.17034848,
+    "PHONE": "(905) 262-4510"
+  },
+  {
+    "ID": 12,
+    "NAME": "Port Colborne Historical and Marine Museum",
+    "MUNICIPALITY": "Port Colborne",
+    "ADDRESS": "280 King St",
+    "LONGITUDE": -79.25186922,
+    "LATITUDE": 42.88762817,
+    "PHONE": "(905)834-7604"
+  },
+  {
+    "ID": 8,
+    "NAME": "Jordan Historical Museum",
+    "MUNICIPALITY": "Lincoln",
+    "ADDRESS": "3802 Main St",
+    "LONGITUDE": -79.37075509,
+    "LATITUDE": 43.1472183,
+    "PHONE": "905-563-2799"
+  },
+  {
+    "ID": 9,
+    "NAME": "Niagara Falls History Museum",
+    "MUNICIPALITY": "Niagara Falls",
+    "ADDRESS": "5810 Ferry St",
+    "LONGITUDE": -79.09125483,
+    "LATITUDE": 43.08861466,
+    "PHONE": "(905)358-5082"
+  },
+  {
+    "ID": 7,
+    "NAME": "Grimsby Museum",
+    "MUNICIPALITY": "Grimsby",
+    "ADDRESS": "6 Murray St",
+    "LONGITUDE": -79.56635328,
+    "LATITUDE": 43.19352583,
+    "PHONE": "(905)945-5292"
+  },
+  {
+    "ID": 1,
+    "NAME": "Grimsby Public Art Gallery",
+    "MUNICIPALITY": "Grimsby",
+    "ADDRESS": "18 Carnegie Lane",
+    "LONGITUDE": -79.55944315,
+    "LATITUDE": 43.19540432,
+    "PHONE": "(905) 945-3246"
+  },
+  {
+    "ID": 11,
+    "NAME": "Niagara Historical Society and Museum",
+    "MUNICIPALITY": "Niagara-on-the-Lake",
+    "ADDRESS": "43 Castlereagh St",
+    "LONGITUDE": -79.0716989,
+    "LATITUDE": 43.25214528,
+    "PHONE": "(905) 468-3912"
+  },
+  {
+    "ID": 14,
+    "NAME": "Welland Historical Museum",
+    "MUNICIPALITY": "Welland",
+    "ADDRESS": "140 King St",
+    "LONGITUDE": -79.25068344,
+    "LATITUDE": 42.98913234,
+    "PHONE": "(905) 732-2215"
+  },
+  {
+    "ID": 5,
+    "NAME": "YMCA of Niagara Children's Discovery Centre",
+    "MUNICIPALITY": "St. Catharines",
+    "ADDRESS": "360 Niagara St N",
+    "LONGITUDE": -79.2245641,
+    "LATITUDE": 43.18308983,
+    "PHONE": "(905) 309-9622"
+  },
+  {
+    "ID": 2,
+    "NAME": "Niagara Falls Art Gallery",
+    "MUNICIPALITY": "Niagara Falls",
+    "ADDRESS": "8058 Oakwood Dr",
+    "LONGITUDE": -79.12128194,
+    "LATITUDE": 43.05930357,
+    "PHONE": "(905) 356-1514"
+  },
+  {
+    "ID": 6,
+    "NAME": "Fort Erie Historical Museum",
+    "MUNICIPALITY": "Fort Erie",
+    "ADDRESS": "402 Ridge Rd",
+    "LONGITUDE": -79.05170716,
+    "LATITUDE": 42.88572025,
+    "PHONE": "(905) 894-5322"
+  },
+  {
+    "ID": 10,
+    "NAME": "Willoughby Historical Museum",
+    "MUNICIPALITY": "Niagara Falls",
+    "ADDRESS": "9935 Niagara River Pkwy",
+    "LONGITUDE": -79.01975466,
+    "LATITUDE": 43.03389196,
+    "PHONE": "(905) 295-4036"
+  },
+  {
+    "ID": 13,
+    "NAME": "St Catharines Museum and Welland Canals Centre",
+    "MUNICIPALITY": "St. Catharines",
+    "ADDRESS": "1932 Welland Canals Pkwy RR 6",
+    "LONGITUDE": -79.19428598,
+    "LATITUDE": 43.15510987,
+    "PHONE": "(905) 984-8880"
+  },
+  {
+    "ID": 17,
+    "NAME": "Ontario Lacrosse Hall of Fame and Museum",
+    "MUNICIPALITY": "St. Catharines",
+    "ADDRESS": "1932 Welland Canals Pkwy RR 6",
+    "LONGITUDE": -79.19433415,
+    "LATITUDE": 43.15546063,
+    "PHONE": "1 (800) 305-5134"
+  },
+  {
+    "ID": 20,
+    "NAME": "Fort Erie Railroad Museum",
+    "MUNICIPALITY": "Fort Erie",
+    "ADDRESS": "400 Central Ave",
+    "LONGITUDE": -78.91746617,
+    "LATITUDE": 42.92287748,
+    "PHONE": "(905) 894-5322"
+  },
+  {
+    "ID": 21,
+    "NAME": "Fergie Jenkins Museum",
+    "MUNICIPALITY": "St. Catharines",
+    "ADDRESS": "67 Commerce Place, Suite 3",
+    "LONGITUDE": -79.26775061,
+    "LATITUDE": 43.14549168,
+    "PHONE": "(905) 688-0880"
+  },
+  {
+    "ID": 22,
+    "NAME": "Battle Ground Hotel Museum",
+    "MUNICIPALITY": "Niagara Falls",
+    "ADDRESS": "6137 Lundy's Lane",
+    "LONGITUDE": -79.09632897,
+    "LATITUDE": 43.09000038,
+    "PHONE": "(905) 358-5082"
+  },
+  {
+    "ID": 23,
+    "NAME": "Niagara Children's Museum",
+    "MUNICIPALITY": "Niagara Falls",
+    "ADDRESS": "8058 Oakwood Dr",
+    "LONGITUDE": -79.12128046,
+    "LATITUDE": 43.05916344,
+    "PHONE": "(905) 356-1514"
+  },
+  {
+    "ID": 24,
+    "NAME": "Mewinzha: First Nations Gallery",
+    "MUNICIPALITY": "Fort Erie",
+    "ADDRESS": "100 Queen St",
+    "LONGITUDE": -78.91669223,
+    "LATITUDE": 42.90879287,
+    "PHONE": "(905) 894-5322"
+  },
+  {
+    "ID": 25,
+    "NAME": "Old Fort Erie",
+    "MUNICIPALITY": "Fort Erie",
+    "ADDRESS": "350 Lakeshore Rd",
+    "LONGITUDE": -78.92395698,
+    "LATITUDE": 42.89351679,
+    "PHONE": "(905) 871-0540"
+  },
+  {
+    "ID": 26,
+    "NAME": "McFarland House",
+    "MUNICIPALITY": "Niagara-on-the-Lake",
+    "ADDRESS": "15927 Niagara Pkwy",
+    "LONGITUDE": -79.06094048,
+    "LATITUDE": 43.23209924,
+    "PHONE": "(905) 468-3322"
+  },
+  {
+    "ID": 27,
+    "NAME": "Laura Secord Homestead",
+    "MUNICIPALITY": "Niagara-on-the-Lake",
+    "ADDRESS": "29 Queenston St",
+    "LONGITUDE": -79.05572281,
+    "LATITUDE": 43.16405063,
+    "PHONE": "(905) 262-4851"
+  },
+  {
+    "ID": 28,
+    "NAME": "Rodman Hall Art Centre",
+    "MUNICIPALITY": "St. Catharines",
+    "ADDRESS": "109 St. Paul Cres",
+    "LONGITUDE": -79.2458824,
+    "LATITUDE": 43.15007492,
+    "PHONE": "(905) 684-2925"
+  },
+  {
+    "ID": 29,
+    "NAME": "Thorold and Beaverdams Historical Society Museum",
+    "MUNICIPALITY": "Thorold",
+    "ADDRESS": "2 Vine St",
+    "LONGITUDE": -79.19853912,
+    "LATITUDE": 43.12606872,
+    "PHONE": "(905) 984-4435"
+  },
+  {
+    "ID": 30,
+    "NAME": "Morningstar Mill",
+    "MUNICIPALITY": "St. Catharines",
+    "ADDRESS": "2714 Decew Rd",
+    "LONGITUDE": -79.26444937,
+    "LATITUDE": 43.11024472,
+    "PHONE": "(905) 688-6050"
+  },
+  {
+    "ID": 31,
+    "NAME": "Niagara Apothecary Museum",
+    "MUNICIPALITY": "Niagara-on-the-Lake",
+    "ADDRESS": "5 Queen St",
+    "LONGITUDE": -79.07085264,
+    "LATITUDE": 43.25511202,
+    "PHONE": "(905) 468-3845"
+  },
+  {
+    "ID": 32,
+    "NAME": "Fort George",
+    "MUNICIPALITY": "Niagara-on-the-Lake",
+    "ADDRESS": "51 Queen's Parade",
+    "LONGITUDE": -79.06813508,
+    "LATITUDE": 43.24696671,
+    "PHONE": "(905) 468-6614"
+  },
+  {
+    "ID": 33,
+    "NAME": "Lincoln & Welland Regiments Museum",
+    "MUNICIPALITY": "Niagara-on-the-Lake",
+    "ADDRESS": "King & Mary Streets",
+    "LONGITUDE": -79.07614505,
+    "LATITUDE": 43.24919242,
+    "PHONE": "(905) 468-0888"
+  },
+  {
+    "ID": 34,
+    "NAME": "Ball's Falls Centre for Conservation",
+    "MUNICIPALITY": "Jordan",
+    "ADDRESS": "3292 Sixth Ave",
+    "LONGITUDE": -79.38603793,
+    "LATITUDE": 43.1331481,
+    "PHONE": "(905) 562-5235"
+  }
+]
